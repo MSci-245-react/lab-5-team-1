226392af2f14ffbdb6e426b8d15da2bc5cfdf7a9
Curtis Tse <curtistlh03@gmail.com>
2024-03-16 17:16:18 -0400

    List restaurants

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,11 +1,22 @@
-import {useEffect} from 'react';
-    export default function RestaurantList({loadRestaurants, restaurants}) {
-      useEffect(() => {
-        loadRestaurants();
-      }, [loadRestaurants]);
-    return ( <ul>
-          {restaurants.map(restaurant => (
-            <li key={restaurant.id}>{restaurant.name}</li>
+import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import {loadRestaurants} from '../store/restaurants/actions';
+
+export function RestaurantList({ loadRestaurants, restaurants }) {
+  useEffect(() => {
+    loadRestaurants();
+  }, [loadRestaurants]);
+  return (<ul>
+    {restaurants.map(restaurant => (
+      <li key={restaurant.id}>{restaurant.name}</li>
     ))} </ul>
-    );
-    };
+  );
+};
+
+const mapStateToProps = state => ({
+  restaurants: state.restaurants.records,
+});
+
+const mapDispatchToProps = {loadRestaurants};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantList);
